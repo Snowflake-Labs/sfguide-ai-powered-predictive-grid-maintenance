c46a16a4fd6368a8e224725ae82c4282ced9641b
Dureti Shemsi <dureti.shemsi@snowflake.com>
2026-01-18 21:18:05 -0800

    Update deploy.sh paths for new repo structure

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# Copyright 2026 Snowflake Inc.
-# SPDX-License-Identifier: Apache-2.0
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 
 ##############################################################################
 # Grid Reliability & Predictive Maintenance - Deployment Script
@@ -702,4 +687,3 @@ echo -e "  📖 Full Guide:     ${YELLOW}docs/guides/DEPLOYMENT_GUIDE.md${NC}"
 echo -e "  📖 Architecture:   ${YELLOW}docs/architecture/ARCHITECTURE.md${NC}"
 echo -e "  📖 Dashboard Guide: ${YELLOW}docs/guides/STREAMLIT_DASHBOARD_GUIDE.md${NC}"
 echo ""
-
